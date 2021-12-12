84b43c020ccbb26e31e12055bec7ffccf871d771
Jan Bretschneider <jan@bretti.net>
2021-12-12 13:22:34 +0100

    Update dependencies to versions which require only minimal code changes

--- src/main/java/io/pivio/view/AppLauncher.java
+++ src/main/java/io/pivio/view/AppLauncher.java
@@ -1,18 +1,12 @@
 package io.pivio.view;
 
-import org.apache.commons.cli.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class AppLauncher {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         SpringApplication.run(AppLauncher.class, args);
     }
 }
